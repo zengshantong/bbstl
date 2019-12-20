7bc5b0772ebc6acfd02a80e64e62d607d01622d7
zst <775272290@qq.com>
2019-12-20 12:13:07 +0800

    project

---
+++ project/src/main/java/zst_gd/bbstl/BbstlApplication.java
@@ -0,0 +1,13 @@
+package zst_gd.bbstl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbstlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbstlApplication.class, args);
+    }
+
+}
---
+++ project/src/test/java/zst_gd/bbstl/BbstlApplicationTests.java
@@ -0,0 +1,13 @@
+package zst_gd.bbstl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbstlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
